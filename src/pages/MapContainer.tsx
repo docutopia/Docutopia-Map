9f40a811acf29614268c8f6fd1da51a73cf91651
Anton Tranelis <mail@antontranelis.de>
2024-02-16 09:00:12 +0100

    rebase docutopia map

--- src/pages/MapContainer.tsx
+++ src/pages/MapContainer.tsx
@@ -4,14 +4,12 @@ import { permissionsApi } from '../api/permissionsApi';
 import { Place, Event, Tag } from '../api/directus';
 import { useEffect, useState } from 'react';
 import {CalendarDaysIcon, MapPinIcon, UserIcon} from '@heroicons/react/20/solid'
-// import { refiBcnApi } from '../api/refiBcnApi';
 
 function MapContainer() {
 
 
   const [placesApi, setPlacesApi] = useState<itemsApi<Place>>();
   const [eventsApi, setEventsApi] = useState<itemsApi<Event>>();
- // const [refiApi, setRefiApi] = useState<refiBcnApi>();
   const [tagsApi, setTagsApi] = useState<itemsApi<Tag>>();
   const [permissionsApiInstance, setPermissionsApiInstance] = useState<permissionsApi>();
   const [updatesApiInstance, setUpdatesApiInstance] = useState<itemsApi<Place>>();
@@ -21,11 +19,10 @@ function MapContainer() {
 
   useEffect(() => {
 
-    setPlacesApi(new itemsApi<Place>('places',"e31de961-6709-4413-a27d-00e59ccfe472"));
-    setEventsApi(new itemsApi<Event>('events', "1837e83e-07f1-44c0-88c0-8e9ea8e597db"));
-    setUpdatesApiInstance(new itemsApi('updates',"b4dd8b6b-80e8-4173-9682-4a5755e7b9cb", undefined, {"latest":{"_eq": true}}));
-    setTagsApi(new itemsApi<Tag>('tags', undefined, "8bf681a4-1b8d-44ba-afba-c6dbf79a769f"));
-  //  setRefiApi(new refiBcnApi('refi'));
+    setPlacesApi(new itemsApi<Place>('places',"2f6f0c3a-614f-4512-94fc-774578660fe0"));
+    setEventsApi(new itemsApi<Event>('events', "bf5c3ae0-3982-43f6-8b02-1b886a368e09"));
+    setUpdatesApiInstance(new itemsApi('updates',"7b3c2a0f-3785-40b5-a578-d8f0c381f10f", undefined, {"latest":{"_eq": true}}));
+    setTagsApi(new itemsApi<Tag>('tags', undefined, "ee933d40-1555-40a5-9fab-7021625116e6"));
     setPermissionsApiInstance(new permissionsApi());
 
   }, []);
@@ -96,8 +93,6 @@ function MapContainer() {
           </div>
         </ItemForm>
       </Layer>
-      {/**       <Layer name='ReFi-BCN' menuIcon={MapPinIcon} menuText='add new place' menuColor='#2E7D32' markerIcon='circle-solid' markerShape='circle' markerDefaultColor='#818583' itemTextField='description' itemLatitudeField='geolocation.lat' itemLongitudeField='geolocation.lon' api={refiApi}> </Layer>
- */}
       <Tags api={tagsApi}></Tags>
       <Permissions api={permissionsApiInstance} adminRole='8ed0b24e-3320-48cd-8444-bc152304e580'></Permissions>
     </UtopiaMap>
